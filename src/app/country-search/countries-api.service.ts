be348534d66e9a626deb79c47ab14ec70ff5fc0b
Максим Рябец <maksaxel2@gmail.com>
2020-11-06 10:41:21 +0200

    feat(country-api-service): add method handle error for method get countries

--- src/app/country-search/countries-api.service.ts
+++ src/app/country-search/countries-api.service.ts
@@ -3,7 +3,7 @@ import {Observable, of} from 'rxjs';
 import {Country} from './country.interface';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {debounceTime, map} from 'rxjs/operators';
+import {catchError, debounceTime, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,24 @@ export class CountriesApiService {
         countries.filter(({ name }) =>
           name.toLocaleLowerCase().startsWith(country.toLocaleLowerCase()))),
       debounceTime(200),
+      catchError(this.handleError<Country[]>('getCountries', []))
     );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
