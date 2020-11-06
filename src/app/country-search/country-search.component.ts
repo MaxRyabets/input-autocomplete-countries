c61e6fefd68909e3d984192010b8dc33a52c86c4
Максим Рябец <maksaxel2@gmail.com>
2020-11-06 15:37:35 +0200

    fix(country-search): fix close result

--- src/app/country-search/country-search.component.ts
+++ src/app/country-search/country-search.component.ts
@@ -1,23 +1,27 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
-import {Observable, of} from 'rxjs';
+import {fromEvent, Observable, of, Subject} from 'rxjs';
 import {Country} from './country.interface';
 import {CountriesApiService} from './countries-api.service';
-import {concatMap, delay, tap} from 'rxjs/operators';
+import {concatMap, delay, filter, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-country-search',
   templateUrl: './country-search.component.html',
   styleUrls: ['./country-search.component.scss']
 })
-export class CountrySearchComponent implements OnInit {
-  private country = '';
-
+export class CountrySearchComponent implements OnInit, AfterViewInit, OnDestroy {
   form = this.formBuilder.group({
     country: [''],
   });
 
+  messageFoundedCountry = '';
+
+  destroy$ = new Subject();
+
   countries: Country[] = [];
+  isClickCountry = false;
+  isDisplayCountrySearch = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -28,17 +32,60 @@ export class CountrySearchComponent implements OnInit {
     this.getValueChangesFromCountry();
   }
 
+  trackByFn(index, item): number {
+    return item.id;
+  }
+
+  onClickCountry(country: string): void {
+    this.isClickCountry = true;
+    this.form.controls.country.setValue(country);
+    this.closeSearchResult();
+  }
+
+  closeSearchResult(): void {
+    this.isDisplayCountrySearch = false;
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(window, 'keyup')
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(() => {
+          const country: string = this.form.get('country').value;
+
+          if (this.countries.length && country.length) {
+            this.isDisplayCountrySearch = true;
+
+            return;
+          }
+
+          this.isDisplayCountrySearch = false;
+        }),
+        delay(200),
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   private getValueChangesFromCountry(): void {
     this.form.get('country')
       .valueChanges
       .pipe(
+        filter(country => country.trim()),
         concatMap(country => {
           return of(country)
             .pipe(
               delay(100),
               concatMap(_ => {
                 return this.getCounty(country).pipe(
-                  tap(countries => this.countries = countries),
+                  tap(countries => {
+                    this.countries = countries;
+                    this.messageFoundedCountry = countries.length ? '' : 'not found';
+                  }),
                 );
               })
             );
